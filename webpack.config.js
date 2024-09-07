beb92ba23549fc26ab92efd6c3fdd8052dc978e1
ItsJady-Taken <davidkhun2004@gmail.com>
2024-09-07 15:21:30 +0700

    Work on a weather app

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,16 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/',
+    entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, './dist'),
         clean: true,
     },
-    Plugins: [
+    plugins: [
         new htmlWebpackPlugin (
             {
-                template: './src/',
+                template: './src/template.html',
             },
         ),
     ],
